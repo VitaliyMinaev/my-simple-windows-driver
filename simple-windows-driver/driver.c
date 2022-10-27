cb3e7d5bf80dfd1fc7a407ad1f573858dcd3ce4e
VitaliyMinaev <bezuglivita123@gmail.com>
2022-10-26 20:53:44 -0700

    Final version

--- simple-windows-driver/driver.c
+++ simple-windows-driver/driver.c
@@ -1,9 +1,34 @@
 #include "driver.h"
 
-UNICODE_STRING DeviceName = RTL_CONSTANT_STRING(L"\\Device\\mydevice-driver");
-UNICODE_STRING SymbolicLinkName = RTL_CONSTANT_STRING(L"\\??\\mydevice-driver-link");
+UNICODE_STRING DeviceName = RTL_CONSTANT_STRING(L"\\Device\\mydevicedriver");
+UNICODE_STRING SymbolicLinkName = RTL_CONSTANT_STRING(L"\\??\\mydevicedriverlink");
 
 PDEVICE_OBJECT DeviceObject = NULL;
+
+NTSTATUS DispatchPassThru(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+	PIO_STACK_LOCATION irpsp = IoGetCurrentIrpStackLocation(Irp);
+	NTSTATUS status = STATUS_SUCCESS;
+
+	switch (irpsp->MajorFunction) {
+	case IRP_MJ_CREATE:
+		DbgPrintEx(0, 0, ("create request \r\n"));
+		break;
+	case IRP_MJ_CLOSE:
+		DbgPrintEx(0, 0, ("create request \r\n"));
+		break;
+	case IRP_MJ_READ:
+		DbgPrintEx(0, 0, ("create request \r\n"));
+		break;
+	default:
+		break;
+	}
+
+	Irp->IoStatus.Information = 0;
+	Irp->IoStatus.Status = status;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
+	return status;
+}
 
 NTSTATUS DriverEntry(
 	_In_ PDRIVER_OBJECT driverObject,
@@ -11,6 +36,8 @@ NTSTATUS DriverEntry(
 ) 
 {
 	DbgPrintEx(0, 0, ("[win-driver]"__FUNCTION__));
+	UNREFERENCED_PARAMETER(registryPath);
+
 	NTSTATUS status;
 
 	driverObject->DriverUnload = DriverUnload;
@@ -32,7 +59,23 @@ NTSTATUS DriverEntry(
 		return status;
 	}
 
+	for (int i = 0; i < IRP_MJ_MAXIMUM_FUNCTION; ++i) {
+		driverObject->MajorFunction[i] = DispatchPassThru;
+	}
+
+	// driverObject->MajorFunction[IRP_MJ_READ] = DispatchCustom;
+	// driverObject->MajorFunction[IRP_MJ_WRITE] = DispatchCustom;
+
+
 	DbgPrintEx(0, 0, ("Driver has been loaded \r\n"));
+
+	PRTL_OSVERSIONINFOW operationSystemData;
+	RtlGetVersion(&operationSystemData);
+	// Print data about os
+	DbgPrintEx(0, 0, "Data about system: windows: %u %u.%u", 
+		(unsigned int)operationSystemData->dwBuildNumber,
+		(unsigned int)operationSystemData->dwMajorVersion,
+		(unsigned int)operationSystemData->dwMinorVersion);
 
 	return status;
 }
